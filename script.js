5ac76a4fe1e8a57dc67bb01f114fab070e3cdffa
Rackz4534543 <153512080+Rackz4534543@users.noreply.github.com>
2024-10-07 19:36:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var links = document.getElementById("navLinks");
+    if (links.style.display === "block") {
+        links.style.display = "none";
+    } else {
+        links.style.display = "block";
+    }
+}
